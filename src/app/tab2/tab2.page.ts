2bddeb140646de28e0e7b1aaa8714adb1a50f306
Raynier Perez Minyety <rayniercraft@gmail.com>
2023-11-06 11:32:15 -0400

    a

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,4 +9,8 @@ export class Tab2Page {
 
   constructor() {}
 
+  numero:number = 0;
+  
+  tabla = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -9,4 +9,21 @@ export class Tab3Page {
 
   constructor() {}
 
+  num1:number = 0;
+  num2:number = 0;
+
+  MostrarFlag:boolean = false;
+
+  numeroMayor:string = "";
+
+  mostrar() {
+    if (this.num1 == this.num2) {
+      this.numeroMayor = "Ambos numero son iguales!";
+    }else if (this.num1 > this.num2) {
+      this.numeroMayor = `El numero mayor es el primer numero (${this.num1})`
+    }else {
+      this.numeroMayor = `El numero mayor es el segundo numero (${this.num2})`
+    }
+    this.MostrarFlag = true;
+  }
 }
